13be1126db5fe3675f68cbd196117fe9fcaee936
陳鈞 <jim60105@gmail.com>
2021-07-17 17:03:34 +0800

    加入專案檔案。

---
+++ AddVideoIDtoMetadata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddVideoIDtoMetadata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
