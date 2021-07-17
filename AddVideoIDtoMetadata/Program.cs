683e917ea32e166e0a333a7d68775d5d0dc8d650
陳鈞 <jim60105@gmail.com>
2021-07-17 18:01:19 +0800

    完成實作

--- AddVideoIDtoMetadata/Program.cs
+++ AddVideoIDtoMetadata/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace AddVideoIDtoMetadata
 {
     class Program
     {
-        static void Main(string[] args)
+        private static BlobContainerClient containerClient;
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create a BlobServiceClient object which will be used to create a container client
+            // Get the container and return a container client object
+            string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING_VTUBER");
+            BlobServiceClient blobServiceClient = new(connectionString);
+            containerClient = blobServiceClient.GetBlobContainerClient("vtuber");
+
+            Console.WriteLine("Listing blobs...");
+
+            // List all blobs in the container
+            await foreach (BlobItem blobItem in containerClient.GetBlobsAsync())
+            {
+                Console.WriteLine($"Blob: {blobItem.Name}");
+
+                BlobClient blobClient = containerClient.GetBlobClient(blobItem.Name);
+                Regex rgx = new Regex(@".*\(([^\(]+)\).*");
+                Match match = rgx.Match(blobItem.Name);
+                string ID = match.Success? match.Groups[1].Value : "";
+
+                Console.WriteLine($"Id: {ID}");
+
+                Dictionary<string, string> tags = new() { { "id", ID } };
+
+                _ = await blobClient.SetTagsAsync(tags);
+                // Cannot modify metadata on archive blobs.
+                //_ = await blobClient.SetMetadataAsync(tags);
+            }
         }
     }
 }
